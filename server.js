e82a712663762cf0c2571411deff310c5644d53f
kathryn-lgtm <kathryn@chdigitals.com.au>
2026-04-10 14:15:52 +1000

    Fix CORS headers for Shopify domain

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.json());
 
 /**
